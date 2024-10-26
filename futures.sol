627b85e7b6b788693d9bca64c109a9c9e7300e4a
Minimega12121 <architdabral1234567890@gmail.com>
2024-10-26 18:37:36 +0530

    updated

--- futures.sol
+++ futures.sol
@@ -38,15 +38,15 @@ contract Futures {
     }
 
     struct FuturesContract {
-        uint256 leverage;
-        uint256 entryPrice;
-        uint256 tokenUnits;
-        uint256 collateral;
-        uint256 timestamp;
+        uint256 leverage;     
+        uint256 entryPrice;   
+        uint256 tokenUnits;   
+        uint256 collateral; 
+        uint256 timestamp; 
     }
 
-    mapping(address => FuturesContract[]) private openPositions;
-    mapping(address => FuturesContract[]) private settledPositions;
+    mapping(address => FuturesContract[]) public openPositions;
+    mapping(address => FuturesContract[]) public settledPositions;
 
     OHLCV[15] public ohlcvQueue; // Fixed-size array for 15 OHLCV values
 
@@ -79,11 +79,11 @@ contract Futures {
     function getOHLCVQueue() public view returns (OHLCV[15] memory) {
         return ohlcvQueue;
     }
-
+                                                                                                   
     function openPosition(uint256 _leverage, uint256 tokenAmount) external {
         require(_leverage >= 1 && _leverage <= 100, "Leverage range not correct");
         require(token.balanceOf(msg.sender) >= tokenAmount, "User must have sufficient balance");
-
+        
         token.transferFrom(msg.sender, address(this), tokenAmount); // take the collateral amount inside the contract
 
         openPositions[msg.sender].push(FuturesContract({
@@ -99,7 +99,7 @@ contract Futures {
         require(positionIndex < openPositions[msg.sender].length, "Invalid position index");
 
         FuturesContract memory position = openPositions[msg.sender][positionIndex];
-        int256 pnl = calculatePnL(position); // Calculate PnL
+        int256 pnl = calculatePnL(positionIndex); // Calculate PnL
 
         if (pnl < 0) {
             uint256 loss = uint256(-pnl); // convert to positive value
@@ -117,10 +117,34 @@ contract Futures {
         }
     }
 
-    function calculatePnL(FuturesContract memory position) public view returns (int256) {
+    function getPositions() external view returns(FuturesContract[] memory) {
+        return openPositions[msg.sender];
+    }
+
+    function calculatePnL(uint256 positionIndex) public view returns (int256) {
+        FuturesContract memory position = openPositions[msg.sender][positionIndex];
         int256 priceDiff = int256(currentMarketPrice) - int256(position.entryPrice);
         int256 pnl = (priceDiff * int256(position.tokenUnits) * int256(position.leverage)) / int256(position.entryPrice);
-        return pnl;
+        return pnl; 
     }
 
 }
+// Dummy data:
+// [
+//     [100, 105, 95, 102, 1500],
+//     [102, 110, 101, 108, 1700],
+//     [108, 112, 107, 109, 1200],
+//     [109, 111, 106, 107, 1800],
+//     [107, 109, 104, 105, 1600],
+//     [105, 107, 102, 103, 1550],
+//     [103, 106, 101, 105, 1650],
+//     [105, 108, 103, 107, 1400],
+//     [107, 110, 106, 109, 1450],
+//     [109, 113, 108, 111, 1750],
+//     [111, 115, 109, 114, 1900],
+//     [114, 117, 112, 115, 2000],
+//     [115, 120, 114, 118, 2100],
+//     [118, 122, 116, 121, 2200],
+//     [121, 125, 119, 123, 2300]
+// ]
+
